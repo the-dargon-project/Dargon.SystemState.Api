f848826d1cd50c2d0c8404783f3c03513c90a8e6
Michael Yu <ItzWarty@gmail.com>
2015-07-29 09:24:08 -0700

    SystemState exposes boolean state getter/setters.

--- SystemState.cs
+++ SystemState.cs
@@ -2,5 +2,8 @@
    public interface SystemState {
       string Get(string key, string defaultValue);
       void Set(string key, string value);
+
+      bool GetBoolean(string key, bool defaultValue);
+      void SetBoolean(string key, bool value);
    }
 }
